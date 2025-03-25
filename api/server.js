8e5765001cf743e2ffec3741520d379fe231be89
vichilotus <tieudoanh@gmail.com>
2025-03-26 03:09:09 +0700

    uncode server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-// See https://github.com/typicode/json-server#module
+/* // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
@@ -29,3 +29,4 @@ server.listen(3000, () => {
 
 // Export the Server API
 module.exports = server
+ */
